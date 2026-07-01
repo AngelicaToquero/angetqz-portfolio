f4518b235d34a89ded24cc9c01e62bff8363ab24
Angelica Toquero <angelicatoquero33@gmail.com>
2026-07-02 00:12:29 +0800

    done images and badges

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -49,9 +49,11 @@ export const Hero = () => {
                         <div className="animate-fade-in">
                             <span className="inline-flex items-center gap-2 px-4 py-2 rounded-full glass text-sm text-primary">
                             <span className="w-2 h-2 bg-primary rounded-full animate-pulse"/>
-                                UI UX Designer 
+                                Front-End Developer
                                 </span>
+                                
                         </div>
+                        
 
                         {/* Headline  */}
                         <div className="space-y-4">
@@ -89,6 +91,34 @@ export const Hero = () => {
                     </div>
 
                     {/* Picture Column */}
+                    <div className='relative animate-fade-in animation-delay-300'>
+                    {/* Profile Image */}
+                    <div className='relative max-w-md mx-auto'>
+                        <div className='absolute inset-0 rounded-3xl bg-linear-to-br from-primary/30
+                        via-transparent to-primary/10 blur-2xl animate-pulse'>
+
+                        </div>
+                            <div className='relative glass rounded-3xl p-2 glow-border'>
+                                <img src="/dp.jpg" alt="Angelica Toquero" 
+                                className='w-full aspect-4/5 object-cover rounded-2xl'/>
+                            {/* Floating Badge */}
+                            <div className='absolute -bottom-4 -right-4 glass rounded-xl
+                            px-4 py-3 animate-float '>
+                                <div className='flex items-center gap-3'>
+                                    <div className='w-3 h-3 bg-green-500 rounded-full animate-pulse'/>
+                                    <span className='text-sm font-medium'>Available for Work</span>
+                                </div>
+                            </div>
+
+                            {/* Status Badge */}
+                            <div className='absolute -top-4 -left-4 glass rounded-xl px-4 py-3 animate-float animation-delay-500'>
+                                    <div className='text-xs text-muted-foreground'>You can call me,</div>
+                                    <div className='text-2xl font-bold text-white'>ANGE!</div>
+                            </div>
+                            
+                            </div>
+                    </div>
+                    </div>
                 </div>
             </div>
     </section>
